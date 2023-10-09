93b46263fc86b67cf9bfa74971face420f9090bb
Wiibleyde <nathan@bonnell.fr>
2023-10-09 13:49:55 +0200

    Default parameter
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -6,5 +6,12 @@ namespace CSharpDiscovery.Quest03
         public double Longitude { get; set; }
         public string? Name { get; set; }
         public static string GoogleMapsUrlTemplate { get; }= "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+
+        public PointOfInterest(string name = "Bordeaux Ynov Campus", double latitude = 44.854186, double longitude = -0.5663056)
+        {
+            Name = name;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
     }
 }
