d2bc303d74330de8c30e82c8bc78402e97d9b929
Wiibleyde <nathan@bonnell.fr>
2023-10-09 11:30:47 +0200

    FromStringToDateTime
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- FromStringToDateTime.cs
+++ FromStringToDateTime.cs
@@ -4,10 +4,7 @@ namespace CSharpDiscovery.Quest02
     {
         public static DateTime FromStringToDateTime(string dateStr)
         {
-            if (dateStr.Contains('h')) {
-                dateStr = dateStr.Replace('h', ':');
-            }
-            return DateTime.Parse(dateStr);
+            return DateTime.ParseExact(dateStr, "dd/MM/yyyy HH:mm", null);
         }
     }
 }
