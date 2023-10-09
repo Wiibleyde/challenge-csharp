4734d4176e4648feb9a5a9e9cee848ab5c4aa221
Wiibleyde <nathan@bonnell.fr>
2023-10-09 14:26:48 +0200

    ToString (the third)
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ Campus.cs
@@ -0,0 +1,14 @@
+using System.Drawing;
+
+namespace CSharpDiscovery.Quest03
+{
+    public class Campus : PointOfInterest
+    {
+        public int Capacity { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{Name} is a campus with a capacity of {Capacity} students";
+        }
+    }
+}
