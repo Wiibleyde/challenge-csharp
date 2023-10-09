8e8343fae933be591c032e7780f08ac64e1b662d
Wiibleyde <nathan@bonnell.fr>
2023-10-09 15:12:21 +0200

    Interface
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ CarInterfaces.cs
@@ -0,0 +1,20 @@
+namespace CSharpDiscovery.Quest04
+{
+    public interface IThermalCar
+    {
+        int FuelLevel { get; }
+
+        void FillUp();
+
+        int GetFuelLevel();
+    }
+
+    public interface IElectricCar
+    {
+        int BatteryLevel { get; }
+
+        void Recharge();
+
+        int GetBatteryLevel();
+    }
+}
