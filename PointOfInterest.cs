a1252fad0479245cc86efbefcc68ace725493efd
Wiibleyde <nathan@bonnell.fr>
2023-10-09 13:41:54 +0200

    Fix non nullable
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -4,7 +4,7 @@ namespace CSharpDiscovery.Quest03
     {
         public double Latitude;
         public double Longitude;
-        public string Name;
+        public string Name = "";
         public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
     }
 }
