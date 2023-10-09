497d2d38cb91222e1496bdb8731c3313f8bc1dcf
Wiibleyde <nathan@bonnell.fr>
2023-10-09 13:43:23 +0200

    Update
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -2,9 +2,9 @@ namespace CSharpDiscovery.Quest03
 {
     public class PointOfInterest
     {
-        public double Latitude;
-        public double Longitude;
-        public string Name = "";
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
         public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
     }
 }
