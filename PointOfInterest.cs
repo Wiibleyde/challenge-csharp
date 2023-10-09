ed75faf4fcc0b6d55e25b7029c2056332494307c
Wiibleyde <nathan@bonnell.fr>
2023-10-09 13:45:49 +0200

    Update first exo
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -4,14 +4,7 @@ namespace CSharpDiscovery.Quest03
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string Name { get; set; }
-        public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/" {
-            get {
-                return GoogleMapsUrlTemplate;
-            }
-            set {
-                GoogleMapsUrlTemplate = value;
-            }
-        }
+        public string? Name { get; set; }
+        public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
     }
 }
