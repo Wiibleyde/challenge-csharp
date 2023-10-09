95ca9e259fc544ed36e3d77d179127d43ad11823
Wiibleyde <nathan@bonnell.fr>
2023-10-09 13:39:35 +0200

    Point of interest
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ PointOfInterest.cs
@@ -0,0 +1,10 @@
+namespace CSharpDiscovery.Quest03
+{
+    public class PointOfInterest
+    {
+        public double Latitude;
+        public double Longitude;
+        public string Name;
+        public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+    }
+}
